63d1b95a9cc48477c4b9efec20255ad23fead6e1
enumura1 <akirayuto0718@gmail.com>
2023-11-12 00:01:13 +0900

    add:light and shadow

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,15 @@ export default function App() {
   return (
     <>
       <Canvas camera={{ position: [2, 1, 2], near: 0.05 }}
-        style={{ background: "#001122" }} >
+        style={{ background: "#001122" }}>
         {/* 3Dモデルの表示 */}
         <group>
           <Model />
         </group>
         {/* 自動横回転 */}
         <OrbitControls autoRotate />
+        {/* ライト */}
+        <directionalLight position={[5, 5, 5]} castShadow />
         {/* 統計情報 */}
         <Stats />
       </Canvas>
